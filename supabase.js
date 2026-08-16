f4ed5da3f4b9bd2bd79074f0baa71546c6c7fa0c
Ian Barreto Lessa <lessaian157@gmail.com>
2026-08-16 14:06:50 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://xcwjqbqinnvnyiktyjbj.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "COLE_SUA_CHAVE_PUBLISHABLE_AQUI";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
